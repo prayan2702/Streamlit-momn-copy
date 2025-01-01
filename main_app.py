a89d897d2a97bcd0d6eb9a54f37be28ed09f6340
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-01 17:29:34 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
+
 # Define the pages in the app
-    pages = {
-        "Momentum Ranking": st.Page("momn_streamlit_app.py", default=True),
-        "Strategy Performance": st.Page("Strategy_performance.py"),
-        "Strategy Tearsheet": st.Page("strategy-tearsheet.py"),
-    }
+pages = {
+    "Momentum Ranking": st.Page("momn_streamlit_app.py", default=True),
+    "Strategy Performance": st.Page("Strategy_performance.py"),
+    "Strategy Tearsheet": st.Page("strategy-tearsheet.py"),
+}
 
-    # Create a navigation bar
-    pg = st.navigation(pages)
+# Create a navigation bar
+pg = st.navigation(pages)
 
-    # Run the navigation bar
-    pg.run()
+# Run the navigation bar
+pg.run()
