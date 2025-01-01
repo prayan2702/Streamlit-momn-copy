ac0c85ce152c8da1d3d3eea76a429b67d1335d4b
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-01 16:53:50 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Define the pages in the app
 pages = {
-    "Page 1": st.Page("page1.py"),
+    "Page 1": st.Page("page1.py", default=True),  # page1.py को डिफ़ॉल्ट पेज के रूप में सेट करें
     "Page 2": st.Page("page2.py"),
     "Page 3": st.Page("page3.py"),
 }
