9b0cc76cbc326bc4e1f2e042c5cae7b9b23c74d4
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-01 17:01:32 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 # Define the pages in the app
 pages = {
-    "Page 1": st.Page("page1.py", default=True),  # page1.py को डिफ़ॉल्ट पेज के रूप में सेट करें
-    "Page 2": st.Page("page2.py"),
-    "Page 3": st.Page("page3.py"),
+    "Momentum Ranking": st.Page("momn_streamlit_app.py", default=True),  # page1.py को डिफ़ॉल्ट पेज के रूप में सेट करें
+    "Strategy Performance": st.Page("Strategy_performance.py"),
+    "Strategy Tearsheet": st.Page("strategy-tearsheet.py"),
 }
 
 # Create a navigation bar
