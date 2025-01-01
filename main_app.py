54439c66706b9505c7acf859dfb8239daf8acefe
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-01 17:01:55 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Define the pages in the app
 pages = {
-    "Momentum Ranking": st.Page("momn_streamlit_app.py", default=True),  # page1.py को डिफ़ॉल्ट पेज के रूप में सेट करें
+    "Momentum Ranking": st.Page("momn_streamlit_app.py", default=True),  # momn_streamlit_app.py को डिफ़ॉल्ट पेज के रूप में सेट करें
     "Strategy Performance": st.Page("Strategy_performance.py"),
     "Strategy Tearsheet": st.Page("strategy-tearsheet.py"),
 }
